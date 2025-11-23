4b2e51586568a82fd030d78cff74eafb6993b7f7
AnnaHort <annahort3d@gmail.com>
2025-11-23 18:07:37 +0200

    add toggle listener

--- js/index.js
+++ js/index.js
@@ -14,3 +14,38 @@ new Swiper(".swiper", {
 		},
 	},
 });
+
+// _____________________________ToggleEvent_____________________________
+const minusButtons = document.querySelectorAll(".minus-quantity");
+const plusButtons = document.querySelectorAll(".plus-quantity");
+const currentQuantity = document.querySelectorAll(".current-quantity-number");
+// current-price-number
+const currentPrice = document.querySelector(".current-price-number");
+const oldPrice = document.querySelector(".current-price-discount");
+// початкова ціна за 1 одиницю товару
+const oneItemPrice = Number(currentPrice.innerHTML);
+const oneItemOldPrice = Number(oldPrice.innerHTML);
+
+minusButtons.forEach(btn => {
+	btn.addEventListener("click", () => {
+		if (Number(currentPrice.innerHTML) > oneItemPrice) {
+			currentPrice.innerHTML = Number(currentPrice.innerHTML) - oneItemPrice;
+			oldPrice.innerHTML = Number(oldPrice.innerHTML) - oneItemOldPrice;
+		}
+		currentQuantity.forEach(val => {
+			if (Number(val.innerHTML) > 1) {
+				val.innerHTML--;
+			}
+		});
+	});
+});
+
+plusButtons.forEach(btn => {
+	btn.addEventListener("click", () => {
+		currentPrice.innerHTML = Number(currentPrice.innerHTML) + oneItemPrice;
+		oldPrice.innerHTML = Number(oldPrice.innerHTML) + oneItemOldPrice;
+		currentQuantity.forEach(val => {
+			val.innerHTML++;
+		});
+	});
+});
